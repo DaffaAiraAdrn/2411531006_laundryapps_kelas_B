8dd352c43ec39a7b8551f5ad26c1bf17e40da1d1
DaffaAiraAdrn <whysoseriou0567@gmail.com>
2025-11-15 21:17:06 +0700

    Praktikum Pekan 6

--- laundryapps/src/confg/Database.java
+++ laundryapps/src/confg/Database.java
@@ -1,23 +1,24 @@
 package confg;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Database {
-	Connection conn;
-	public static Connection koneksi() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/laundry_apps", "root", "");
-			System.out.println("anda berhasil login");
-			return conn;
-		}
-		catch(Exception e) {
-			JOptionPane.showMessageDialog(null, e);
-			return null;
-		}
-	}
-	public static void main(String args[]) {
-		Database.koneksi();
-	}
-
+    private static Connection conn;
+    public static Connection koneksi() {
+        if (conn == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/laundry_apps", "root", "");
+                System.out.println("Koneksi database berhasil!");
+            } catch (ClassNotFoundException | SQLException e) {
+                JOptionPane.showMessageDialog(null, "Koneksi database gagal:\n" + e.getMessage());
+            }
+        }
+        return conn;
+    }
+    public static void main(String[] args) {
+    	koneksi();
+    }
 }
