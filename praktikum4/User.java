475d9c5116ea2fbaf9b060a2dacbab9887018b7f
DaffaAiraAdrn <whysoseriou0567@gmail.com>
2025-12-24 21:28:00 +0700

    Pekan 4 (Praktikum pekan 6 & 7 berada di folder laundryapps)

---
+++ praktikum4/User.java
@@ -0,0 +1,52 @@
+package praktikum4;
+
+public class User {
+	private String name;
+	
+	public User() {
+		this.name = "Pengguna Umum";
+	}
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public void viewBookDetails(Book book) {
+		System.out.println("Judul: " + book.getTitle());
+		System.out.println("Penulis: " + book.getAuthor());
+		System.out.println("Tersedia: " + (book.isAvailable()? "Ya" : "Tidak"));
+		
+		
+		if (book instanceof Novel) {
+			Novel novel = (Novel) book;
+			System.out.println("Genre: " + novel.getGenre());
+			
+		}else if(book instanceof Magazine) {
+			Magazine magazine = (Magazine) book;
+			System.out.println("Kategori: " + magazine.getKategori());
+		}else if(book instanceof Textbook) {
+			Textbook textbook = (Textbook) book;
+			System.out.println("Bidang Studi: " + textbook.getBidangStudi());
+		}
+	}
+	
+	public void borrowBook(Book book) {
+		if(book.isAvailable()) {
+			book.borrowBook();
+			System.out.println("Buku \"" + book.getTitle() + "\" berhasil dipinjam oleh " + this.name);
+		}else {
+			System.out.println("Maaf " + this.name + ", buku \"" + book.getTitle() + "\" sedang tidak tersedia.");
+		}
+	}
+	
+	public void returnBook(Book book) {
+		if(!book.isAvailable()) {
+			book.returnBook();
+			System.out.println("Buku \"" + book.getTitle() + "\" berhasil dikembalikan.");
+		}else {
+			System.out.println("Buku \"" + book.getTitle() + "\" sudah tersedia.");
+		}
+	}
+	
+	
+
+}
